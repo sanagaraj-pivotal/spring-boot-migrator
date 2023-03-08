3dff001ab0b96dc8f0cb6a61fb272b71c1c405a6
sanagaraj-pivotal <sanagaraj@pivotal.io>
2023-03-08 15:08:24 +0000

    Ping ...

--- components/sbm-recipes-boot-upgrade/src/test/java/org/springframework/sbm/boot/upgrade_27_30/report/helper/JohnzonDependencyHelperSectionTest.java
+++ components/sbm-recipes-boot-upgrade/src/test/java/org/springframework/sbm/boot/upgrade_27_30/report/helper/JohnzonDependencyHelperSectionTest.java
@@ -69,4 +69,38 @@ public class JohnzonDependencyHelperSectionTest {
                                                                 
                                 """);
     }
+
+    @Test
+    public void helperWorksWithoutExplicitVersion() {
+
+        ProjectContext context = TestProjectContext.buildProjectContext()
+                .withMavenRootBuildFileSource("""
+                        <?xml version="1.0" encoding="UTF-8"?>
+                        <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+                            <modelVersion>4.0.0</modelVersion>
+                            <parent>
+                               <groupId>org.springframework.boot</groupId>
+                               <artifactId>spring-boot-starter-parent</artifactId>
+                               <version>2.7.1</version>
+                               <relativePath/>
+                             </parent>
+                            <groupId>com.example</groupId>
+                            <artifactId>dummy-root</artifactId>
+                            <version>0.1.0-SNAPSHOT</version>
+                            <packaging>pom</packaging>
+                            <dependencies>
+                                <dependency>
+                                    <groupId>org.apache.johnzon</groupId>
+                                    <artifactId>johnzon-core</artifactId>
+                                </dependency>
+                            </dependencies>
+                                                
+                        </project>
+                        """)
+                .build();
+
+        SpringBootUpgradeReportTestSupport
+                .generatedSection("JSON-B")
+                .fromProjectContext(context);
+    }
 }
